03009f0ee34543dfed67b649a079e8595adcd199
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-08-29 20:29:43 +0530

    defined Board constructor

--- Board.cpp
+++ Board.cpp
@@ -1,1 +1,24 @@
 #include "Board.h"
+
+#include <string>
+#include <sstream>
+
+
+/*
+	constuctor which reads input from text file and creates 2-D matrix
+*/
+Board::Board(std::ifstream& file)
+{
+	std::string line;
+	while (std::getline(file, line))
+	{
+		board.push_back(std::vector<int>());
+
+		std::istringstream is(line);
+		int i = 0;
+		while (is >> i)
+			board.back().push_back(i);
+	}
+}
+
+
