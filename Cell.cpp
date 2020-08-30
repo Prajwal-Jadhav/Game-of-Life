e14687e383704c6c3a4211dcc83b50566957cd8e
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-08-30 19:16:43 +0530

    Added constructor

---
+++ Cell.cpp
@@ -0,0 +1,10 @@
+/* This file contains definitions of members of Cell class */
+
+#include "Cell.h"
+
+// initially we want both states to be same
+Cell::Cell(int state) : old_state(state), new_state(state) {};
+
+
+
+
