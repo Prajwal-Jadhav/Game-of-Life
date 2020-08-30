4e8fa8ec646928850ba5b2d3d2eb7a9a68ea2a98
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-08-30 19:19:00 +0530

    Added functions accessing states

--- Cell.cpp
+++ Cell.cpp
@@ -5,6 +5,13 @@
 // initially we want both states to be same
 Cell::Cell(int state) : old_state(state), new_state(state) {};
 
+int Cell::get_old()
+{
+	return old_state;
+}
 
-
+int Cell::get_new()
+{
+	return new_state;
+}
 
