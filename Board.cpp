08195e8c6dcd5237c7c9feb473b7f18caff0fd51
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-09-01 15:05:11 +0530

    added method for counting alive neighbours of a cell

--- Board.cpp
+++ Board.cpp
@@ -37,7 +37,51 @@ void Board::display_board(std::ostream& os)
 		os << std::endl;
 	}
 
+	
+
 	os << "\n\n" << std::flush;
+
+	os << count_neighbours(13, 0) << std::endl;
+}
+
+/*
+	function for counting state of neighbouring eight cells
+	if conditions are for checking, so that we do not access 
+	elements out of index of vector
+	particularly important for elements on the edge of matrix
+*/
+std::size_t Board::count_neighbours(row i, col j)
+{
+	std::size_t count = 0;
+
+	row row_last_index = board.size() - 1;
+	col col_last_index = board[0].size() - 1;
+
+	if (i != 0)
+		count += board[i - 1][j].get_old();
+
+	if (j != 0)
+		count += board[i][j - 1].get_old();
+
+	if (i != row_last_index)
+		count += board[i + 1][j].get_new();
+
+	if (j != col_last_index)
+		count += board[i][j + 1].get_new();
+
+	if (i != 0 && j != 0)
+		count += board[i - 1][j - 1].get_old();
+
+	if (i != 0 && j != col_last_index)
+		count += board[i - 1][j + 1].get_old();
+
+	if (i != row_last_index && j != col_last_index)
+		count += board[i + 1][j + 1].get_new();
+
+	if (i != row_last_index && j != 0)
+		count += board[i + 1][j - 1].get_new();
+
+	return count;
 }
 
 
